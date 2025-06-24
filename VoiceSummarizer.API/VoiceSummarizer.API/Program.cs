0458c5bd18bfafcfc6996986c07f2d160e3d62a8
Esti Shulman <st0548411288@gmail.com>
2025-06-24 17:43:04 +0300

    התחברות למשתני סביבה

--- VoiceSummarizer.API/VoiceSummarizer.API/Program.cs
+++ VoiceSummarizer.API/VoiceSummarizer.API/Program.cs
@@ -1,10 +1,175 @@
+//using Microsoft.EntityFrameworkCore;
+//using DL.Contexts;
+//using BL.IService;
+//using BL.Services;
+//using DL.IRepositories;
+//using DL.Repository;
+//using System.Text.Json;
+//using System.Text.Json.Serialization;
+//using BL;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.IdentityModel.Tokens;
+//using System.Text;
+//using Microsoft.OpenApi.Models;
+//using Amazon.S3;
+//using Amazon;
+//using Amazon.Extensions.NETCore.Setup;
+//using Amazon.Runtime;
+//using Microsoft.Extensions.DependencyInjection;
+
+
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Logging.AddConsole();
+
+//// הזרקת שירותים
+//builder.Services.AddScoped<ICategoryService, CategoryService>();
+//builder.Services.AddScoped<ISummaryService, SummaryService>();
+//builder.Services.AddScoped<IUserFileService, UserFileService>();
+//builder.Services.AddScoped<IUserService, UserService>();
+//builder.Services.AddScoped<IS3Service,S3Service>();
+//builder.Services.AddHttpClient<IAssemblyAiService, AssemblyAiService>();
+//builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+////builder.Services.AddAWSService<IAmazonS3>();
+
+//var awsOptions = builder.Configuration.GetSection("AWS");
+//string region = awsOptions["Region"] ?? "us-east-1";
+//string accessKeyId = awsOptions["AccessKey"];
+//string secretAccessKey = awsOptions["SecretKey"];
+
+//if (string.IsNullOrEmpty(accessKeyId) || string.IsNullOrEmpty(secretAccessKey))
+//{
+//throw new InvalidOperationException("AWS credentials are missing from appsettings.json.");
+//}
+
+//builder.Services.AddSingleton<IAmazonS3>(sp =>
+//{
+//    var config = new AmazonS3Config
+//    {
+//        RegionEndpoint = RegionEndpoint.GetBySystemName(region)
+//    };
+
+//    var credentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
+//    return new AmazonS3Client(credentials, config);
+//});
+
+
+
+//// הזרקת רפוזיטוריז
+//builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+//builder.Services.AddScoped<ISummaryRepository, SummaryRepository>();
+//builder.Services.AddScoped<IUserFileRepository, UserFileRepository>();
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+//// הוספת AutoMapper
+//builder.Services.AddAutoMapper(typeof(AppMappingProfile));
+
+//// הוספת קונפיגורציה של Json
+//builder.Services.AddControllers().AddJsonOptions(options =>
+//{
+//    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+//    options.JsonSerializerOptions.WriteIndented = true;
+//});
+
+//// הוספת Swagger
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen(options =>
+//{
+//    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+//    {
+//        Scheme = "Bearer",
+//        BearerFormat = "JWT",
+//        In = ParameterLocation.Header,
+//        Name = "Authorization",
+//        Description = "Bearer Authentication with JWT Token",
+//        Type = SecuritySchemeType.Http
+//    });
+//    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+//    {
+//        {
+//            new OpenApiSecurityScheme
+//            {
+//                Reference = new OpenApiReference
+//                {
+//                    Id = "Bearer",
+//                    Type = ReferenceType.SecurityScheme
+//                }
+//            },
+//            new List<string>()
+//        }
+//    });
+//});
+
+//// הוספת CORS
+//var allowedOrigins = "_allowedOrigins";
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy(name: allowedOrigins,
+//        policy =>
+//        {
+//            policy.WithOrigins(
+//                    "http://localhost:5173",// זה הפורט של React שלך
+//                    "https://practicum-react-frontside.onrender.com",
+//                    "http://localhost:4200"
+//                )
+//                .AllowAnyHeader()
+//                .AllowAnyMethod();
+//        });
+//});
+
+//// הגדרת MySQL
+//builder.Services.AddDbContext<VoiceSummarizerDbContext>(options =>
+//    options.UseMySql(
+//        builder.Configuration.GetConnectionString("DefaultConnection"),
+//        new MySqlServerVersion(new Version(7, 0, 11)) // שימי כאן את גרסת MySQL הנכונה
+//    ));
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//    .AddJwtBearer(options =>
+//    {
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuer = true,
+//            ValidateAudience = true,
+//            ValidateLifetime = true,
+//            ValidateIssuerSigningKey = true,
+//            ValidIssuers = builder.Configuration.GetSection("JWT:Issuer").Get<string[]>(),
+//            ValidAudiences = builder.Configuration.GetSection("JWT:Audience").Get<string[]>(),
+//            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+//        };
+
+//    });
+
+//var app = builder.Build();
+
+//// קונפיגורציה של Swagger בסביבת פיתוח
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//// השתמש ב-CORS (החלק הזה חייב להיות לפני UseAuthorization)
+//app.UseCors(allowedOrigins);
+
+//// קונפיגורציה של HTTP ו־Authorization
+//app.UseHttpsRedirection();
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
 using Microsoft.EntityFrameworkCore;
 using DL.Contexts;
 using BL.IService;
 using BL.Services;
 using DL.IRepositories;
 using DL.Repository;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 using BL;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -13,32 +178,29 @@ using System.Text;
 using Microsoft.OpenApi.Models;
 using Amazon.S3;
 using Amazon;
-using Amazon.Extensions.NETCore.Setup;
 using Amazon.Runtime;
 using Microsoft.Extensions.DependencyInjection;
-
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddConsole();
 
-// הזרקת שירותים
+// שירותים
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ISummaryService, SummaryService>();
 builder.Services.AddScoped<IUserFileService, UserFileService>();
 builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IS3Service,S3Service>();
+builder.Services.AddScoped<IS3Service, S3Service>();
 builder.Services.AddHttpClient<IAssemblyAiService, AssemblyAiService>();
-builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
-//builder.Services.AddAWSService<IAmazonS3>();
 
-var awsOptions = builder.Configuration.GetSection("AWS");
-string region = awsOptions["Region"] ?? "us-east-1";
-string accessKeyId = awsOptions["AccessKey"];
-string secretAccessKey = awsOptions["SecretKey"];
+// קריאת משתני סביבה או appsettings
+var awsSection = builder.Configuration.GetSection("AWS");
+string region = Environment.GetEnvironmentVariable("AWS_REGION") ?? awsSection["Region"] ?? "us-east-1";
+string accessKeyId = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID") ?? awsSection["AccessKey"];
+string secretAccessKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY") ?? awsSection["SecretKey"];
 
 if (string.IsNullOrEmpty(accessKeyId) || string.IsNullOrEmpty(secretAccessKey))
 {
-throw new InvalidOperationException("AWS credentials are missing from appsettings.json.");
+    throw new InvalidOperationException("Missing AWS credentials from env or appsettings.");
 }
 
 builder.Services.AddSingleton<IAmazonS3>(sp =>
@@ -47,30 +209,27 @@ builder.Services.AddSingleton<IAmazonS3>(sp =>
     {
         RegionEndpoint = RegionEndpoint.GetBySystemName(region)
     };
-
     var credentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
     return new AmazonS3Client(credentials, config);
 });
 
-
-
-// הזרקת רפוזיטוריז
+// רפוזיטוריז
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ISummaryRepository, SummaryRepository>();
 builder.Services.AddScoped<IUserFileRepository, UserFileRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
-// הוספת AutoMapper
+// AutoMapper
 builder.Services.AddAutoMapper(typeof(AppMappingProfile));
 
-// הוספת קונפיגורציה של Json
+// JSON
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     options.JsonSerializerOptions.WriteIndented = true;
 });
 
-// הוספת Swagger
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -99,7 +258,7 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 
-// הוספת CORS
+// CORS
 var allowedOrigins = "_allowedOrigins";
 builder.Services.AddCors(options =>
 {
@@ -107,7 +266,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy.WithOrigins(
-                    "http://localhost:5173",// זה הפורט של React שלך
+                    "http://localhost:5173",
                     "https://practicum-react-frontside.onrender.com",
                     "http://localhost:4200"
                 )
@@ -116,49 +275,44 @@ builder.Services.AddCors(options =>
         });
 });
 
-// הגדרת MySQL
+// MySQL
 builder.Services.AddDbContext<VoiceSummarizerDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(7, 0, 11)) // שימי כאן את גרסת MySQL הנכונה
+        new MySqlServerVersion(new Version(7, 0, 11))
     ));
+
+// JWT
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 })
-    .AddJwtBearer(options =>
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuers = builder.Configuration.GetSection("JWT:Issuer").Get<string[]>(),
-            ValidAudiences = builder.Configuration.GetSection("JWT:Audience").Get<string[]>(),
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
-        };
-
-    });
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuers = builder.Configuration.GetSection("JWT:Issuer").Get<string[]>(),
+        ValidAudiences = builder.Configuration.GetSection("JWT:Audience").Get<string[]>(),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+    };
+});
 
 var app = builder.Build();
 
-// קונפיגורציה של Swagger בסביבת פיתוח
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// השתמש ב-CORS (החלק הזה חייב להיות לפני UseAuthorization)
 app.UseCors(allowedOrigins);
-
-// קונפיגורציה של HTTP ו־Authorization
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
