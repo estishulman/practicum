0e42b2103f70e04c29ea0547410cc4b2a3ae40e3
Esti Shulman <st0548411288@gmail.com>
2025-06-25 00:29:54 +0300

    update the url of server

--- react/client-side/src/services/Api.tsx
+++ react/client-side/src/services/Api.tsx
@@ -2,7 +2,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5047/api', // כתובת בסיס
+  //baseURL: 'http://localhost:5047/api', // כתובת בסיס
+  baseURL:'https://practicum-server7.onrender.com'
 })
 
 api.interceptors.request.use(
